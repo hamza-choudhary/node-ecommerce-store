c3742df24efe91fc871517de73528a1e7bce138f
hamza-choudhary <humzach700@gmail.com>
2023-02-25 18:52:29 +0500

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,111 @@
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/user')
+
+exports.getLogin = (req, res, next) => {
+	let message = req.flash('error')
+
+	if (message.length > 0) {
+		message = message[0]
+	} else {
+		message = null
+	}
+
+	res.render('auth/login', {
+		path: '/login',
+		pageTitle: 'Login',
+		errorMessage: message, // whatever we stored in the name of error will come here
+	})
+}
+
+exports.getSignup = (req, res, next) => {
+	let message = req.flash('error')
+	if (message.length > 0) {
+		message = message[0]
+	} else {
+		message = null
+	}
+	res.render('auth/signup', {
+		path: '/signup',
+		pageTitle: 'Signup',
+		errorMessage: message,
+	})
+}
+
+exports.postLogin = (req, res, next) => {
+	const email = req.body.email
+	const password = req.body.password
+
+	User.findOne({ email: email })
+		.then((user) => {
+			if (!user) {
+				req.flash('error', 'Invalid email or password')
+				return res.redirect('/login')
+			}
+
+			bcrypt
+				.compare(password, user.password) //it will only go to catch block if there is error !IMPORTANT password is not matchin is not an error
+				.then((doMatch) => {
+					//doMatch will be bolean value
+					if (doMatch) {
+						req.session.isLoggedIn = true
+						req.session.user = user
+						return req.session.save((err) => {
+							console.log(err)
+							res.redirect('/')
+						})
+					}
+					req.flash('error', 'Invalid email or password.')
+					res.redirect('/login')
+				})
+				.catch((err) => {
+					console.log(err)
+					res.redirect('/login')
+				})
+		})
+		.catch((err) => console.log(err))
+}
+
+exports.postSignup = (req, res, next) => {
+	const email = req.body.email
+	const password = req.body.password
+	const confirmPassword = req.body.confirmPassword
+
+	User.findOne({ email: email })
+		.then((userDoc) => {
+			//user already exist
+			if (userDoc) {
+				req.flash(
+					'error',
+					'E-Mail exists already, please pick a different one.'
+				)
+				return res.redirect('/signup')
+			}
+
+			return bcrypt //it is async and will return promise
+				.hash(password, 12)
+				.then((hashedPassword) => {
+					const user = new User({
+						email: email,
+						password: hashedPassword,
+						cart: { items: [] },
+					})
+
+					return user.save()
+				})
+				.then((result) => {
+					//user is siged up
+					res.redirect('/login')
+				})
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+}
+
+exports.postLogout = (req, res, next) => {
+	req.session.destroy((err) => {
+		console.log(err)
+		res.redirect('/')
+	})
+}
